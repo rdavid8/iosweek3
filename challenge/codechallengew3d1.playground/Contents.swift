8c905c97669062eac0c3d19395a4524915db6e9f
Ryan David <rdavid8@gmail.com>
2016-02-15 21:50:41 -0800

    attempt at code challenge

---
+++ challenge/codechallengew3d1.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func countWords(string: String)-> Int {
+    var count = 0
+    var sentence = [String]()
+    
+    for character in string.sentence {
+    let number = String(character)
+    sentence.append(number)
+
+//not sure where to go from here..
